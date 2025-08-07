c7b3c493b762eac8b3c93be5b1989e0cd641fa8a
root <carlaciarr@gmail.com>
2025-08-07 06:44:36 +0000

    edit 7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -19,7 +19,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	}
 
 	/* visit left subtree, then node, then right subtree */
-	binary_tree_preorder(tree->left, func);
+	binary_tree_inorder(tree->left, func);
 	func(tree->n);
-	binary_tree_preorder(tree->right, func);
+	binary_tree_inorder(tree->right, func);
 }
