3a99399ed4580b7ec8567af9f35e27bd529eb206
root <carlaciarr@gmail.com>
2025-08-07 04:48:28 +0000

    change null parent structure

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,10 +13,10 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_left_node = malloc(sizeof(binary_tree_t));
+
 	if (parent == NULL)
 		return (NULL);
-
-	binary_tree_t *new_left_node = malloc(sizeof(binary_tree_t));
 
 	if (new_left_node == NULL)
 		return (NULL);
