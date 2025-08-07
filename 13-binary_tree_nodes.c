04fcee9b44218edcda1ef5adea8bd619d04a1e12
root <carlaciarr@gmail.com>
2025-08-07 10:07:33 +0000

    edit 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -19,6 +19,12 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 	}
 
+	/* if tree is the root, return 0 */
+	if (tree->parent == NULL)
+	{
+		return (0);
+	}
+
 	left_node = binary_tree_nodes(tree->left);
 	right_node = binary_tree_nodes(tree->right);
 
