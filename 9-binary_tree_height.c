6baa920f35aeb73e443bde266a872aa8891b2d8d
root <carlaciarr@gmail.com>
2025-08-07 07:14:32 +0000

    edit return

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -28,5 +28,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
                return (1 + left_height);
 	   else
 	       return (1 + right_height); */
-	return (1 + (left_height > right_height ? left_height : right_height));
+	return (left_height > right_height ? left_height : right_height);
 }
