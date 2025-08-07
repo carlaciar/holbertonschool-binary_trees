91bd56b646b6a04f1fec387b6e2ead4965b1fdf5
root <carlaciarr@gmail.com>
2025-08-07 10:05:13 +0000

    edit 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -19,13 +19,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 	}
 
-	if (tree->left != NULL || tree->right != NULL)
-	{
-		return (1);
-	}
-
 	left_node = binary_tree_nodes(tree->left);
 	right_node = binary_tree_nodes(tree->right);
 
-	return (left_node + right_node);
+	return (1 + (left_node + right_node));
 }
